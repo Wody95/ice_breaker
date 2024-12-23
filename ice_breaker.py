e71e37a51912e45794cddbc904d03f5d21a350cc
Wody95 <kwu0626@gmail.com>
2024-12-23 09:51:06 +0900

    add: dotenv

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,6 +1,11 @@
 import os
+from dotenv import load_dotenv
 
 
 if __name__ == "__main__":
+
+    # 환경변수 로드
+    load_dotenv()
     print("Hello, World!")
-    print(os.environ.get("OPENAI_API_KEY"))
+    # print(os.environ.get("OPENAI_API_KEY"))
+    print(os.getenv("OPENAI_API_KEY"))

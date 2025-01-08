be9b657623914901ceba5455348ece8315a4b58c
wody95 <kwu0626@gmail.com>
2025-01-09 08:59:32 +0900

    update ice_breaker.py
    - apply linked in mock data
    - update summary template
    - change llm model
    - change search name

--- ice_breaker.py
+++ ice_breaker.py
@@ -8,18 +8,19 @@ from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 
 def ice_break_with(name: str) -> str:
     linkedin_username = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
     given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
+    3. answer in Korean language
     """
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
 
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+    llm = ChatOpenAI(temperature=0, model_name="gpt-4o-mini")
 
     chain = summary_prompt_template | llm
 
@@ -32,4 +33,4 @@ if __name__ == "__main__":
     load_dotenv()
 
     print("Ice Breaker Enter")
-    ice_break_with(name="Eden Marco")
+    ice_break_with(name="Eden Marco Udemy")
